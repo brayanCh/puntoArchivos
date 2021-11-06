a94f8778b1460beb1a8c5fa5a741c221d63dc8fc
Brayan S. Chavarro Leon <brayanchavarro081@gmail.com>
2021-11-05 21:46:26 -0500

    script guardar cambios

---
+++ actualizar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp ~/.vimrc ./
+cp ~/.xinitrc ./
+cp ~/.Xdefaults ./
+
+cp ~/.config/bspwm/bspwmrc ./.config/bspwm/
+cp ~/.config/kitty/kitty.conf ./.config/kitty
+cp ~/.config/sxhkd/sxhkdrc ./.config/sxhkd
+cp ~/.config/picom.conf ./.config/
+cp ~/.config/ranger/* ./.config/ranger
+
---
+++ scriptReflejar.sh
@@ -0,0 +1,1 @@
+xrandr --output LVDS1 --rate 60 --mode 1920x1080 --fb 1920x1080 --panning 1920x1080* --output HDMI1 --mode 1920x1080 --same-as LVDS1
