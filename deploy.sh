d8b8bc49dc5f02c3089d2ec25688ec448e93a846
brayanCh <brayanchavarro081@gmail.com>
2023-12-04 16:29:32 -0500

    fix script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-cp -r in_dot_config/* ~/.config
-cp -r in_home/* ~/
+cp -r in_dot_config/bspwm ~/.config
+cp -r in_dot_config/sway ~/.config
+cp -r in_dot_config/kitty ~/.config
+cp -r in_dot_config/nvim ~/.config
+cp -r in_dot_config/picom.conf ~/.config
+cp -r in_dot_config/rofi ~/.config
+cp -r in_dot_config/sxhkd ~/.config
+
+cp -r in_home/.zshrc ~/
